4e3c0ede0b9a1e5d928ce34af3008bf833bcc09b
MikeJK9 <98841936+MikeJK9@users.noreply.github.com>
2026-04-17 20:37:04 -0400

    No more errors in recursiveCast, should work in theory.

--- Oasis/src/Oasis/Methods.java
+++ Oasis/src/Oasis/Methods.java
@@ -1,9 +1,10 @@
 package Oasis;
+
 import java.util.Optional;
 
-public class Methods  <U extends Concepts.UnaryExpression<?, ?, ?, ?>, B extends Concepts.BinaryExpression<?, ?, ?, ?>>{
+public class Methods  <U extends UnaryExpression<?, ?>, B extends BinaryExpression<?, ?, ?>>{
 
-
+    //determine if we're casting from binary, unary, or other into Type T
     public <T> T recursiveCast(Expression other, long Category) {
         if (other.getCategory() == ExpressionCategory.UnExp.value) {
             return castUnary(other, Category);
@@ -31,25 +32,26 @@ if constexpr (std::is_same_v<T, Expression>)
 
 
     private <T> T castBinary(Expression other, long Category) {
+        if(other.getCategory() != Category){
+            return null;
+        }
         Expression otherGeneralized = other.Generalize();
         B otherBinaryExpression = (B) otherGeneralized;
         Expression specializedMostSigOp = otherBinaryExpression.getMostSigOp();
         Expression specializedLeastSigOp = otherBinaryExpression.getLeastSigOp();
         if (specializedLeastSigOp != null && specializedMostSigOp != null) {
-            //TODO: return std::make_unique<T>(*specializedMostSigOp, *specializedLeastSigOp);
-            return Optional.empty();
+            return (T) new BinaryExpression<Expression,Expression, Expression>(specializedMostSigOp, specializedLeastSigOp);
         }
         if (other.getCategory() != ExpressionCategory.Commutative.value) {
-            return Optional.empty();
+            return null;
         }
-        B otherWithSwappedOps = otherBinaryExpression.SwapOperands();
+        var otherWithSwappedOps = otherBinaryExpression.swapOperands();
         specializedMostSigOp = otherWithSwappedOps.getMostSigOp();
         specializedLeastSigOp = otherWithSwappedOps.getLeastSigOp();
         if (specializedLeastSigOp != null && specializedMostSigOp != null) {
-            //TODO: return std::make_unique<T>(*specializedMostSigOp, *specializedLeastSigOp);
-            return Optional.empty();
+            return (T) new BinaryExpression<Expression,Expression, Expression>(specializedMostSigOp, specializedLeastSigOp);
         }
-        return Optional.empty();
+        return null;
     }
 
     private <T> T castUnary(Expression other, long Category) {

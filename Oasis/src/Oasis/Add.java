2d35952932b0da25957fed12691f6542b7203a6d
MikeJK9 <98841936+MikeJK9@users.noreply.github.com>
2026-04-19 19:48:17 -0400

    Began the add class's Simplify function

--- Oasis/src/Oasis/Add.java
+++ Oasis/src/Oasis/Add.java
@@ -6,5 +6,24 @@ public class Add <AugendT extends Expression, AddendT extends Expression> extend
         super(mostSigOp, leastSigOp);
     }
 
+    public Expression Simplify(){
+        Expression SimplifiedAugend;
+        Expression SimplifiedAddend;
+        if(getMostSigOp() != null) {
+            SimplifiedAugend = getMostSigOp().Simplify();
+        }
+        else{
+            SimplifiedAugend = null;
+        }
+        if(getLeastSigOp() != null) {
+            SimplifiedAddend = getLeastSigOp().Simplify();
+        }
+        else{
+            SimplifiedAddend = null;
+        }
+        Add<Expression,Expression> SimplifiedAdd = new Add<Expression,Expression>(SimplifiedAddend, SimplifiedAddend);
+
+    }
+
 }
 

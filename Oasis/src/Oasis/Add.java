5acae8b1d98c8d67852076e0caf6f62bb2a31f5e
MikeJK9 <98841936+MikeJK9@users.noreply.github.com>
2026-04-19 19:34:59 -0400

    Began add class, wrote constructor.

--- Oasis/src/Oasis/Add.java
+++ Oasis/src/Oasis/Add.java
@@ -1,5 +1,10 @@
 package Oasis;
 
-public class Add {
-    
+public class Add <AugendT extends Expression, AddendT extends Expression> extends BinaryExpression<Add<?,?>, AugendT, AddendT> {
+
+    Add(AugendT mostSigOp, AddendT leastSigOp) {
+        super(mostSigOp, leastSigOp);
+    }
+
 }
+
